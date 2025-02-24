a10a1416c49749c6b162993983d48343d94ca29f
yomiyow <romeoquinones4@gmail.com>
2025-02-24 11:12:18 +0800

    feat(auth): implement route protection middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json(
+        { message: 'Unauthorized - No token provided' }
+      );
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json(
+        { message: 'Unauthorized - Invalid token' }
+      );
+    }
+
+    const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(404).json(
+        { message: 'User not found' }
+      );
+    }
+
+    req.user = user;
+    next();
+
+  } catch (error) {
+    res.status(500).json(
+      { message: 'Internal Server Error', error: error.message }
+    );
+  }
+}
