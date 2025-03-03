43513a26f067aef6f0a1817c85c8a591f0da3436
yomiyow <romeoquinones4@gmail.com>
2025-03-03 22:50:33 +0800

    feat(setting): implement theme selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,12 @@ import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 
 import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
 import { useEffect } from "react";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -27,7 +29,7 @@ const App = () => {
   console.log({ authUser });
 
   return (
-    < div >
+    < div data-theme={theme}>
       <Navbar />
 
       <Routes>
