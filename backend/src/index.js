52b2527fe2a5e82976a57950ec7d4b07b3f279d3
yomiyow <romeoquinones4@gmail.com>
2025-04-11 11:01:38 +0800

    feat: update package configurations and add production support

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,14 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import { app, server } from './lib/socket.js';
 import cors from 'cors';
+import path from 'path';
 
 import authRoute from './routes/auth.route.js';
 import messageRoute from './routes/message.route.js';
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +22,14 @@ app.use(cors({
 
 app.use('/api/auth', authRoute);
 app.use('/api/messages', messageRoute);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}}`);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,9 @@ import { axiosInstance } from '../lib/axios';
 import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = (import.meta.env.MODE === 'development')
+  ? 'http://localhost:3000'
+  : '/';
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
