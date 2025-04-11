b59e3eb6faa896b8b971491d1aa0e3e5d72a7f99
yomiyow <romeoquinones4@gmail.com>
2025-04-11 09:42:41 +0800

    feat(chat): filter incoming messages to only update for selected user

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -58,6 +58,9 @@ export const useChatStore = create((set, get) => ({
     const socket = useAuthStore.getState().socket;
 
     socket.on('newMessage', (newMessage) => {
+      const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
       set({
         messages: [...get().messages, newMessage]
       });
